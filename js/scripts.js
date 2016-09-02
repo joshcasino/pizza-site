4cfc605bde8488f1ce610807aa7aa3e780b0fab8
Josh Casuccio <joshcasuccio@Joshs-MBP.domain>
2016-09-02 14:18:39 -0700

    fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,48 @@
 //backend
-function Pizza(pieSize, toppings) {
-  this.pieSize = pieSize;
-  this.toppings = toppings;
+function Pizza(size) {
+  this.size = size;
+  this.toppings = [];
 };
 
-Pizza.prototype.price = function() {
-  var baseCost = 9
+Pizza.prototype.price = function(size) {
+  var size =  parseInt($("input:radio[name=pieSize]:checked").val());
+  console.log(size);
   var prices = []
-    if (this.toppings === "pepperoni") {
-        prices.push(baseCost = baseCost + 3)
-        console.log(prices);
-    }
-    if (this.toppings === "mushrooms") {
-        prices.push(baseCost = baseCost + 2)
-    }
-    if (this.toppings === "peppers") {
-        prices.push(baseCost = baseCost + 2)
-    }
-    if (this.pieSize === "large") {
-        prices.push(baseCost = baseCost + 4)
-    } else if (this.pieSize === "medium") {
-        prices.push(baseCost = baseCost + 2)
-    } else {
-      return baseCost;
-    }
-    console.log(baseCost);
 
+
+  if (this.size === "13") {
+      prices.size
+  } else if (this.size === "11") {
+      prices.size
+  } else {
+    // console.log(cost);
+      prices.size;
+  }
+return size;
 };
-
 //ui logic
 $(document).ready(function() {
   $("#pizzaForm").submit(function(event) {
     event.preventDefault();
-    var pieSize = $("input:radio[name=pieSize]:checked").val();
-    console.log(pieSize);
-    var toppings = $("input:checkbox[name=toppings]:checked").val();
-    var myPizza = new Pizza(pieSize, toppings);
+      size = $("input:radio[name=pieSize]:checked").val();
+      // console.log(size);
+
+    var pizzaToppings = [];
+    $("input:checkbox[name=toppings]:checked").each(function() {
+
+    pizzaToppings.push($(this).val());
+      console.log(pizzaToppings);
+      total = 0
+      for (i = 0; i <= pizzaToppings.length; i++) {
+        total += parseInt(pizzaToppings[i]);
+      }
+    // return parseInt(pizzaToppings * 2);
+
+
+
+
+    var myPizza = new Pizza(size, pizzaToppings);
     $("#yourCost").text("$" + myPizza.price() + ".00");
+    });
   });
 });
